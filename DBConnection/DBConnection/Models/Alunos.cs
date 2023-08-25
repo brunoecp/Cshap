e5d4a84b726a74ec8fd834e64f0c01cd19e26467
brunoecp <rm95315@fiap.com.br>
2023-08-25 12:11:49 -0300

    acesso ao banco de dados com c#

---
+++ DBConnection/DBConnection/Models/Alunos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DBConnection.Models
+{
+    public class Alunos
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string nome { get; set; }   
+    }
+}
---
+++ DBConnection/DBConnection/Persistence/OracleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DBConnection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBConnection.Persistence
+{
+    public class OracleDbContext : DbContext
+    {
+        public DbSet<Alunos> alunos { get; set; }
+
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
