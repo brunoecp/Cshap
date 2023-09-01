dbb20f58042ff45d8ec2031be0a30e6e1a0a8a76
brunoecp <rm95315@fiap.com.br>
2023-09-01 09:44:26 -0300

    interfaces

---
+++ DBConnection/DBConnection/Persistence/Repository/AlunoRepository.cs
@@ -0,0 +1,70 @@
+﻿using DBConnection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBConnection.Persistence.Repository
+{
+    public class AlunoRepository : IAlunoRepository
+    {
+        private readonly OracleDbContext _dbContext;
+
+
+
+        public AlunoRepository(OracleDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+        public Alunos GetById(int id)
+        {
+            return _dbContext.alunos.Find(id);
+        }
+
+
+
+        public IEnumerable<Alunos> GetAll()
+        {
+            return _dbContext.alunos.ToList();
+        }
+
+
+
+        public void Add(Alunos entity)
+        {
+            _dbContext.alunos.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+
+
+        public void Update(Alunos entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+
+
+        public void Delete(Alunos entity)
+        {
+            _dbContext.alunos.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void add(Alunos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove(Alunos entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(Alunos entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DBConnection/DBConnection/Persistence/Repository/IAlunoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DBConnection.Models;
+
+namespace DBConnection.Persistence.Repository
+{
+    public interface IAlunoRepository
+    {
+
+        Alunos GetById(int id);
+        IEnumerable<Alunos> GetAll();
+        void add(Alunos entity);
+        void remove(Alunos entity);
+        void update(Alunos entity);
+    }
+}
